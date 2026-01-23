99b261dbc3e4181d2955ad233db703522fe039cc
Junyong Choi <dmsql6829@gmail.com>
2026-01-23 10:37:45 -0500

    start coding week02

--- Week01/q5.py
+++ Week01/q5.py
@@ -1,3 +1,6 @@
 userAge = input("Enter your age: ")
 userAge = int(userAge) + 22
 print("Now showing the shop items filtered by age ", userAge)
+
+
+# git add . && git commit -m "add lab 01" && git push
